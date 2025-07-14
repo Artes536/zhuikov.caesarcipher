35f6172c00cb51f2eb6367a968c4e42c17612ef8
Семён <arteser212fv@gmail.com>
2025-07-14 13:52:55 +0500

    Написана базовая логика шифрования

--- src/main/java/com/example/cipher/CaesarCipher.java
+++ src/main/java/com/example/cipher/CaesarCipher.java
@@ -1,6 +1,6 @@
 package com.example.cipher;
 
-public class CaesarCipher implements Cipher{
+public class CaesarCipher implements Cipher {
     private char[] alphabet;
 
     public CaesarCipher(char[] alphabet) {
@@ -9,11 +9,38 @@ public class CaesarCipher implements Cipher{
 
     @Override
     public String encrypt(String text, int shift) {
-        return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            stringBuilder.append(encryptChar(c, shift));
+        }
+        return stringBuilder.toString();
     }
 
     @Override
     public String decrypt(String encryptedText, int shift) {
-        return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : encryptedText.toCharArray()){
+            stringBuilder.append(decryptChar(c,shift));
+        }
+        return stringBuilder.toString();
+    }
+
+    private char encryptChar(char symbol, int shift) {
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] == symbol) {
+                int newIndex = (i + shift) % alphabet.length;
+                return alphabet[newIndex];
+            }
+        }
+        return symbol;
+    }
+    private char decryptChar(char symbol, int shift) {
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] == symbol) {
+                int newIndex = (i - shift) % alphabet.length;
+                return alphabet[newIndex];
+            }
+        }
+        return symbol;
     }
 }
